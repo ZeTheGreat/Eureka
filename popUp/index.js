2c68aafcb757aa2c970d244dd3105683347d2313
Victor-stack22 <victor.pintassilgo@gmail.com>
2024-05-09 14:50:14 -0300

    salvando e recuperando informações do localstorage

--- popUp/index.js
+++ popUp/index.js
@@ -3,15 +3,9 @@ const textArea = document.querySelector("#note-to-add");
 const duties = document.querySelector("#duties");
 const selectTask = document.querySelector("#task");
 
-submitButton.addEventListener("click", () => {
-  // chrome.runtime.openOptionsPage();
-  chrome.runtime.sendMessage({ message: "messageSent", data: textArea.value });
-});
-
 submitButton.addEventListener("click", function () {
-  submitButton.disabled = true;
-
   let valor = duties.value;
+  let inputValue = textArea.value;
   let dadosExistente = localStorage.getItem("informacoes");
   let informacoes;
 
@@ -21,15 +15,58 @@ submitButton.addEventListener("click", function () {
     informacoes = {};
   }
 
-  let timestamp = Date.now();
-  informacoes[timestamp] = valor;
+  // Usando o valor do duties como chave e o valor do note-to-add como valor no localStorage
+  informacoes[valor] = inputValue;
 
   localStorage.setItem("informacoes", JSON.stringify(informacoes));
   let option = document.createElement("option");
-  option.value = timestamp; // Use o timestamp como valor da option
+  option.value = valor; // Use o valor do duties como valor da option
   option.text = valor;
   selectTask.add(option);
   alert("Tarefa salva com sucesso!");
+});
 
-  submitButton.disabled = false;
+// Adiciona o ouvinte de evento para o evento de mudança no select
+selectTask.addEventListener("change", function() {
+  // Obtém o valor selecionado no select
+  const selectedValue = selectTask.value;
+
+  // Recupera a informação correspondente no localStorage
+  const savedData = localStorage.getItem("informacoes");
+  if (savedData) {
+    const tasks = JSON.parse(savedData);
+    
+    // Exibe a informação correspondente no campo textarea
+    const selectedTask = tasks[selectedValue];
+    if (selectedTask) {
+      textArea.value = selectedTask;
+    } else {
+      textArea.value = ""; // Limpa o textarea se não houver informação correspondente
+    }
+  }
+});
+
+// Recupera os dados salvos no localStorage e preenche o campo de entrada de tarefas
+function carregarTarefas() {
+  const savedData = localStorage.getItem("informacoes");
+  if (savedData) {
+    const tasks = JSON.parse(savedData);
+    
+    // Limpa as opções existentes no select
+    selectTask.innerHTML = "";
+    
+    // Adiciona as novas opções
+    Object.keys(tasks).forEach(duty => {
+      const option = document.createElement('option');
+      option.value = duty; // Use a chave como valor da option
+      option.text = duty; // Use a chave como texto da option
+      selectTask.add(option);
+    });
+  }
+}
+
+
+// Adiciona o ouvinte de evento para o carregamento da extensão
+document.addEventListener("DOMContentLoaded", function () {
+  carregarTarefas();
 });
