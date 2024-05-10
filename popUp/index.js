26d2e7244a939e858c1ba2803156f362161595c1
Victor-stack22 <victor.pintassilgo@gmail.com>
2024-05-10 12:22:26 -0300

    adicionando botao de deletar tarefas

--- popUp/index.js
+++ popUp/index.js
@@ -2,6 +2,7 @@ const submitButton = document.querySelector("#submit-button");
 const textArea = document.querySelector("#note-to-add");
 const duties = document.querySelector("#duties");
 const selectTask = document.querySelector("#task");
+const deleteButton = document.querySelector("#delete");
 
 submitButton.addEventListener("click", function () {
   let valor = duties.value;
@@ -12,14 +13,14 @@ submitButton.addEventListener("click", function () {
   if (dadosExistente) {
     informacoes = JSON.parse(dadosExistente);
   } else {
-    informacoes = {};
+    informacoes = {}; // Se não houver dados salvos, inicializa um novo objeto vazio para armazenar as informações
   }
 
   // Usando o valor do duties como chave e o valor do note-to-add como valor no localStorage
   informacoes[valor] = inputValue;
 
-  localStorage.setItem("informacoes", JSON.stringify(informacoes));
-  let option = document.createElement("option");
+  localStorage.setItem("informacoes", JSON.stringify(informacoes)); // Salva o objeto atualizado no localStorage.
+  let option = document.createElement("option"); 
   option.value = valor; // Use o valor do duties como valor da option
   option.text = valor;
   selectTask.add(option);
@@ -69,4 +70,34 @@ function carregarTarefas() {
 // Adiciona o ouvinte de evento para o carregamento da extensão
 document.addEventListener("DOMContentLoaded", function () {
   carregarTarefas();
+});
+
+deleteButton.addEventListener("click", function () {
+  // Obtém o valor selecionado no select
+  const selectedValue = selectTask.value;
+
+  // Recupera os dados do localStorage
+  let savedData = localStorage.getItem("informacoes");
+
+  if (savedData) {
+    // Transforma os dados do localStorage em um objeto JavaScript
+    let tasks = JSON.parse(savedData);
+
+    // Remove a tarefa selecionada
+    delete tasks[selectedValue];
+
+    // Atualiza os dados no localStorage
+    localStorage.setItem("informacoes", JSON.stringify(tasks));
+
+    // Atualiza as opções do select
+    carregarTarefas();
+
+    // Limpa o textarea
+    textArea.value = "";
+
+    // Exibe uma mensagem de sucesso
+    alert("Tarefa deletada com sucesso!");
+  } else {
+    alert("Nenhuma tarefa para deletar!");
+  }
 });
